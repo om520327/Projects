588cf01860cebc94d3ff85f2022000372be0cfaf
om520327 <om520327@dal.ca>
2023-07-24 19:58:06 -0300

    getURLsFromHTML test and functionality

--- JavaScripthttpnetworkng/WebCrawl/src/crawl.js
+++ JavaScripthttpnetworkng/WebCrawl/src/crawl.js
@@ -1,3 +1,32 @@
+const { JSDOM } = require("jsdom")
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === '/') {
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`error with relative url: ${err.message}`)
+            }
+
+        } else {
+            try {
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`error with absoulte url: ${err.message}`)
+            }
+
+        }
+
+    }
+    return urls
+}
+
 function normailzeURL(urlString) {
     const urlObj = new URL(urlString)
 
@@ -9,5 +38,6 @@ function normailzeURL(urlString) {
 }
 
 module.exports = {
-    normailzeURL
+    normailzeURL,
+    getURLsFromHTML
 }
--- JavaScripthttpnetworkng/WebCrawl/src/crawl.test.js
+++ JavaScripthttpnetworkng/WebCrawl/src/crawl.test.js
@@ -1,4 +1,4 @@
-const { normailzeURL } = require('./crawl.js')
+const { normailzeURL, getURLsFromHTML } = require('./crawl.js')
 const { test, expect } = require('@jest/globals')
 
 test('normailzeURL strip protocol https', () => {
@@ -26,5 +26,69 @@ test('normailzeURL strip protocol http', () => {
     const input = 'http://heep.hepp.dev/path/'
     const actual = normailzeURL(input)
     const expected = 'heep.hepp.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML absolute', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="https://heep.hepp.dev/path/">
+        Hepp.dev Blog
+        </a>
+    </body>
+</html>`
+    const inputBaseURL = "https://heep.hepp.dev/path/"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://heep.hepp.dev/path/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML relative', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="/path/">
+        Hepp.dev Blog
+        </a>
+    </body>
+</html>`
+    const inputBaseURL = "https://heep.hepp.dev"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://heep.hepp.dev/path/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML absoulte and relative', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="https://heep.hepp.dev/path1/">
+            Hepp.dev Blog p1
+        </a>
+        <a href="/path2/">
+            Hepp.dev Blog p2
+        </a>
+    </body>
+</html>
+`
+    const inputBaseURL = "https://heep.hepp.dev"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://heep.hepp.dev/path1/", "https://heep.hepp.dev/path2/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML invalid', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="invalid">
+        Invalid URL
+        </a>
+    </body>
+</html>`
+    const inputBaseURL = "https://heep.hepp.dev"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = []
     expect(actual).toEqual(expected)
 })
