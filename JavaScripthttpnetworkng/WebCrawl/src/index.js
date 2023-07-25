142bb4a18a4f8f2a0364a5728372912a08021873
om520327 <om520327@dal.ca>
2023-07-25 17:10:36 -0300

    crawl page with error handling

--- JavaScripthttpnetworkng/WebCrawl/src/index.js
+++ JavaScripthttpnetworkng/WebCrawl/src/index.js
@@ -1,1 +1,27 @@
-console.log("hi")
+const { crawlPage } = require('./crawl.js')
+/* 
+REASON FOR < 3:
+first arg:
+is interperter, first arg to any program
+is the name of program 
+second arg:
+is the name of our code (entry port file full path)
+third argument:
+is one we are actually passing into
+our program
+*/
+function index() {
+    if (process.argv.length < 3) {
+        console.log("no website provided")
+        process.exit(1)
+    }
+    if (process.argv.length > 3) {
+        console.log("to0 many command like args")
+        process.exit(1)
+    }
+    const baseURL = process.argv[2]
+    console.log(`crawl baby crawl URL:${baseURL}`)
+    crawlPage(baseURL)
+}
+
+index()
