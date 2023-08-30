d7d788c8e6761ee376588b6b975a66168e592b69
om520327 <om520327@dal.ca>
2023-08-30 16:42:24 -0300

    finished consumer

---
+++ Microservices/python/src/converter/convert/to_mp3.py
@@ -0,0 +1,45 @@
+import pika, json, tempfile, os
+from bson.ojectid import ObjectId
+import moviepy.editor
+
+def start(message, fs_videos, fs_mp3s, channel):
+    message = json.loads(message)
+    # creates temp file in temp directory which we can use to write our 
+    # video data to  
+    tf = tempfile.NamedTemporaryFile()
+    # video contents 
+    out = fs_videos.get(ObjectId(message["video_fid"]))
+
+    tf.write(out.read())
+    # create audio from temp vidoe file
+    audio = moviepy.editor.VideoFileClip(tf.name).audio
+    tf.close()
+
+    # write audio to the file 
+    tf_path = tempfile.gettempdir() + f"/{message['video_fid']}.mp3"
+    audio.write_audiofile(tf_path)
+    # save the file to mongo
+    f = open(tf_path, "rb")
+    data = f.read()
+    fid = fs_mp3s.put(data)
+    f.close()
+    os.remove(tf_path)
+
+    message["mp3_fid"] = str(fid)
+
+    # put new message on mp3 q
+
+    try:
+        channel.basic_publish(
+            exchange="",
+            routing_key=os.environ.get("MP3_QUEUE"),
+            body=json.dumps(message),
+            # make sure msg is persisted till processed
+            properties=pika.BasicProperties(
+                delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+            ),
+        )
+    except Exception as err:
+        fs_mp3s.delete(fid)
+        return "failed to publish msg"
+
