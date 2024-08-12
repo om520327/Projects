6c34487a56135c7b8dcdca0c006df892d1206706
SocialShams <omar.shams@socialstore.app>
2024-08-12 14:59:05 -0300

    connecting redux store to nextjs/ connecting sidebarcollapsed and darkmode functionality

--- inventory-mngmt/src/app/dashboardwrapper.tsx
+++ inventory-mngmt/src/app/dashboardwrapper.tsx
@@ -1,18 +1,53 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import Navbar from "@/app/(components)/Navbar";
 import Sidebar from "@/app/(components)/Sidebar";
+import StoreProvider, { useAppSelector } from "./redux";
+import state from "@/state";
 
-const Dashboardwrapper = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
+  {
+    /* if in dark mode "dark" needs to be added to the html element same with "light" */
+  }
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.add("light");
+    }
+  });
+
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
   return (
-    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+    <div
+      className={`${
+        isDarkMode ? "dark" : "light"
+      } flex bg-gray-50 text-gray-900 w-full min-h-screen`}
+    >
       <Sidebar />
       <main
-        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}
+        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${
+          isSidebarCollapsed ? "md:pl-24" : "md:pl-72"
+        }`}
       >
         <Navbar />
         {children}
       </main>
     </div>
+  );
+};
+
+const Dashboardwrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    /* Connecting redux store to nextjs app */
+
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 };
 
