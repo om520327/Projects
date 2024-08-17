d9b755e0739b76c04614f32873839a7a12d10514
SocialShams <omar.shams@socialstore.app>
2024-08-17 14:50:15 -0300

    backend work (setting up config)

--- inventory-mngmt/server/src/index.ts
+++ inventory-mngmt/server/src/index.ts
@@ -4,3 +4,24 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+
+/* CONFIG */
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+
+/* SERVER */
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(` Server running on port: ${port}`);
+});
