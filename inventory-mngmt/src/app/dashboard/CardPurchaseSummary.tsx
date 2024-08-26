47fec45aa35b2ca82dfa31462a1111b40a4143f6
SocialShams <omar.shams@socialstore.app>
2024-08-26 16:34:28 -0300

    purchasesummarycard FE done and connected to BE

--- inventory-mngmt/src/app/dashboard/CardPurchaseSummary.tsx
+++ inventory-mngmt/src/app/dashboard/CardPurchaseSummary.tsx
@@ -2,6 +2,14 @@ import { useGetDashboardMetricsQuery } from "@/state/api";
 import { TrendingDown, TrendingUp } from "lucide-react";
 import numeral from "numeral";
 import React from "react";
+import {
+  AreaChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+  Area,
+} from "recharts";
 
 const CardPurchaseSummary = () => {
   //redux tool kit only makes one call to "useGetDashboardMetricsQuery()" so we dont worry about multiple calls
@@ -10,20 +18,22 @@ const CardPurchaseSummary = () => {
   const purchaseData = data?.purchaseSummary || [];
   const lastDataPoint = purchaseData[purchaseData.length - 1] || null;
   return (
-    <div className=" flex flex-col justify-between row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-white shadow-md rounded-2xl">
+    <div className="flex flex-col justify-between row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-white shadow-md rounded-2xl">
       {isLoading ? (
         <div className="m-5">Loading...</div>
       ) : (
         <>
-          {" "}
           {/* HEADER */}
           <div>
             <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
               Purchase Summary
             </h2>
+            <hr />
           </div>
+
           {/* BODY */}
           <div>
+            {/* BODY HEADER */}
             <div className="mb-4 mt-7 px-7">
               <p className="text-xs text-gray-400">Purchased</p>
               <div className="flex items-center">
@@ -50,6 +60,36 @@ const CardPurchaseSummary = () => {
                 )}
               </div>
             </div>
+            {/* CHART */}
+            <ResponsiveContainer width="100%" height={200} className="p-2">
+              <AreaChart
+                data={purchaseData}
+                margin={{ top: 0, right: 0, left: -50, bottom: 45 }}
+              >
+                <XAxis dataKey="date" tick={false} axisLine={false} />
+                <YAxis tickLine={false} tick={false} axisLine={false} />
+                <Tooltip
+                  formatter={(value: number) => [
+                    `$${value.toLocaleString("en")}`,
+                  ]}
+                  labelFormatter={(label) => {
+                    const date = new Date(label);
+                    return date.toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    });
+                  }}
+                />
+                <Area
+                  type="linear"
+                  dataKey="totalPurchased"
+                  stroke="#8884d8"
+                  fill="#8884d8"
+                  dot={true}
+                />
+              </AreaChart>
+            </ResponsiveContainer>
           </div>
         </>
       )}
