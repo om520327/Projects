36f4091c60f0a606605131e83ffce79237896ada
SocialShams <omar.shams@socialstore.app>
2024-08-28 16:37:19 -0300

    expensesummary card done

--- inventory-mngmt/src/app/dashboard/CardExpenseSummary.tsx
+++ inventory-mngmt/src/app/dashboard/CardExpenseSummary.tsx
@@ -2,6 +2,7 @@ import {
   ExpenseByCategorySummary,
   useGetDashboardMetricsQuery,
 } from "@/state/api";
+import { TrendingUp } from "lucide-react";
 import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
 
 type ExpenseSums = {
@@ -11,6 +12,7 @@ const colors = ["#00C49F", "#0088FE", "#FFBB28"];
 
 const CardExpenseSummary = () => {
   const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
+  const expenseSummary = dashboardMetrics?.expenseSummary[0];
 
   const expenseByCategorySummary =
     dashboardMetrics?.expenseByCategorySummary || [];
@@ -82,6 +84,40 @@ const CardExpenseSummary = () => {
               </div>
             </div>
             {/* LABELS */}
+            <ul className="flex flex-col justify-around items-center xl:items-start py-5 gap-3">
+              {expenseCategories.map((entry, index) => (
+                <li
+                  key={`legend-${index}`}
+                  className="flex items-center text-xs"
+                >
+                  <span
+                    className="mr-2 w-3 h-3 rounded-full"
+                    style={{ backgroundColor: colors[index % colors.length] }}
+                  ></span>
+                  {entry.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* FOOTER */}
+          <div>
+            <hr />
+            {expenseSummary && (
+              <div className="mt-3 flex justify-between items-center px-7 mb-4">
+                <div className="pt-2">
+                  <p className="text-sm">
+                    Average:{" "}
+                    <span className="font-semibold">
+                      ${expenseSummary.totalExpenses.toFixed(2)}
+                    </span>
+                  </p>
+                </div>
+                <span className="flex items-center mt-2">
+                  <TrendingUp className="mr-2 text-green-500" />
+                  30%
+                </span>
+              </div>
+            )}
           </div>
         </>
       )}
