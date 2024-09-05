4731471d1c588c289b272ea3197472169ef4fc3d
SocialShams <omar.shams@socialstore.app>
2024-09-05 17:33:52 -0300

    async getProducts function

--- inventory-mngmt/server/src/controllers/productController.ts
+++ inventory-mngmt/server/src/controllers/productController.ts
@@ -2,3 +2,25 @@ import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
+export const getProducts = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    //searchbar
+    const search = req.query.search?.toString();
+    //getting products data
+    const products = await prisma.products.findMany({
+      // (case for searching certain products if needed if search string not there all products will get fetched)
+      where: {
+        name: {
+          contains: search,
+        },
+      },
+    });
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching Products" });
+  }
+};
