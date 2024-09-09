3b408c8a4685573372f8e758b75e4baa5eb6d07c
SocialShams <omar.shams@socialstore.app>
2024-09-09 14:15:27 -0300

    inv page done

--- inventory-mngmt/src/app/inventory/page.tsx
+++ inventory-mngmt/src/app/inventory/page.tsx
@@ -1,7 +1,34 @@
 "use client";
 
 import { useGetProductsQuery } from "@/state/api";
-
+import Header from "@/app/(components)/Header";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+/* when using datagrid columns and rows must be specified, in our case the rows are the products
+so we only need ot deal with the columns */
+const columns: GridColDef[] = [
+  { field: "productId", headerName: "ID", width: 90 },
+  { field: "name", headerName: "Product Name", width: 200 },
+  {
+    field: "price",
+    headerName: "Price",
+    width: 110,
+    type: "number",
+    valueGetter: (value, row) => `$${row.price}`,
+  },
+  {
+    field: "rating",
+    headerName: "Rating",
+    width: 110,
+    type: "number",
+    valueGetter: (value, row) => (row.rating ? row.rating : "N/A"),
+  },
+  {
+    field: "stockQuantity",
+    headerName: "Stock Quantity",
+    width: 150,
+    type: "number",
+  },
+];
 const Inventory = () => {
   const { data: products, isError, isLoading } = useGetProductsQuery();
 
@@ -18,6 +45,14 @@ const Inventory = () => {
   return (
     <div className="flex flex-col">
       <Header name="Inventory" />
+      <DataGrid
+        rows={products}
+        columns={columns}
+        /* must tell it to use row productID or it will use the row id for our data property */
+        getRowId={(row) => row.productId}
+        checkboxSelection
+        className="bg-white shadow rounded-lg border border-gray-200 mt-5 !text-gray-700"
+      />
     </div>
   );
 };
