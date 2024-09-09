117bf6af385b066f19251b3727f297ce433bb65a
SocialShams <omar.shams@socialstore.app>
2024-09-09 13:50:51 -0300

    header component

--- inventory-mngmt/src/app/inventory/page.tsx
+++ inventory-mngmt/src/app/inventory/page.tsx
@@ -4,7 +4,22 @@ import { useGetProductsQuery } from "@/state/api";
 
 const Inventory = () => {
   const { data: products, isError, isLoading } = useGetProductsQuery();
-  return <div>Inventory</div>;
+
+  if (isLoading) {
+    return <div className="py-4">LOADING....</div>;
+  }
+
+  if (isError || !products) {
+    return (
+      <div className="text-center text-red-500 py-4">Can't get Products</div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col">
+      <Header name="Inventory" />
+    </div>
+  );
 };
 
 export default Inventory;
