b86b45514d312627e6eef3b9bf08f6c50c1c2999
SocialShams <omar.shams@socialstore.app>
2024-09-12 15:39:29 -0300

    users page done

---
+++ inventory-mngmt/src/app/users/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useGetUsersQuery } from "@/state/api";
+import Header from "@/app/(components)/Header";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+/* when using datagrid columns and rows must be specified, in our case the rows are the users
+so we only need ot deal with the columns */
+const columns: GridColDef[] = [
+  { field: "userId", headerName: "ID", width: 90 },
+  { field: "name", headerName: "Name", width: 200 },
+  { field: "email", headerName: "Email", width: 200 },
+];
+const Users = () => {
+  const { data: users, isError, isLoading } = useGetUsersQuery();
+
+  if (isLoading) {
+    return <div className="py-4">LOADING....</div>;
+  }
+
+  if (isError || !users) {
+    return <div className="text-center text-red-500 py-4">Can't get Users</div>;
+  }
+
+  return (
+    <div className="flex flex-col">
+      <Header name="Users" />
+      <DataGrid
+        rows={users}
+        columns={columns}
+        /* must tell it to use row productID or it will use the row id for our data property */
+        getRowId={(row) => row.userId}
+        checkboxSelection
+        className="bg-white shadow rounded-lg border border-gray-200 mt-5 !text-gray-700"
+      />
+    </div>
+  );
+};
+
+export default Users;
