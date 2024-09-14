b4704137ae7596c3c77962e67f8f227b47e4c94e
SocialShams <omar.shams@socialstore.app>
2024-09-14 14:29:06 -0300

    working out settings page

---
+++ inventory-mngmt/src/app/settings/page.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import Header from "@/app/(components)/Header";
+
+type UserSetting = {
+  label: string;
+  value: string | boolean;
+  type: "text" | "toggle";
+};
+
+const mockSettings: UserSetting[] = [
+  { label: "Username", value: "Heeep", type: "text" },
+  { label: "Email", value: "Heeep@heep.com", type: "text" },
+  { label: "Notification", value: true, type: "toggle" },
+  { label: "Dark Mode", value: false, type: "toggle" },
+  { label: "Language", value: "English", type: "text" },
+];
+
+const Settings = () => {
+  const [userSettings, setUserSettings] = useState<UserSetting[]>(mockSettings);
+
+  const handleToggleChange = (index: number) => {
+    const settingsCopy = [...userSettings];
+    settingsCopy[index].value = !settingsCopy[index].value as boolean;
+    setUserSettings(settingsCopy);
+  };
+
+  return (
+    <div className="w-full">
+      <Header name="User Settings" />
+      <div className="overflow-x-auto mt-5 shadow-md"></div>
+    </div>
+  );
+};
+
+export default Settings;
