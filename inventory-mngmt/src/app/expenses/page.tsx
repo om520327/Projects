5a25a6e6e7a0390835818b8e87edeaeda73dad52
SocialShams <omar.shams@socialstore.app>
2024-09-16 15:31:50 -0300

    still flushing out expenses page

--- inventory-mngmt/src/app/expenses/page.tsx
+++ inventory-mngmt/src/app/expenses/page.tsx
@@ -1,7 +1,103 @@
-import React from "react";
+"use client";
+
+import { useGetExpensesByCategoryQuery } from "@/state/api";
+import { useMemo, useState } from "react";
+import Header from "@/app/(components)/Header";
 
 const Expenses = () => {
-  return <div>Expenses</div>;
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+  const {
+    data: expensesData,
+    isLoading,
+    isError,
+  } = useGetExpensesByCategoryQuery();
+  const expenses = useMemo(() => expensesData ?? [], [expensesData]);
+
+  const classNames = {
+    label: "block text-sm font-medium text-gray-700",
+    selectInput:
+      "mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md",
+  };
+
+  if (isLoading) {
+    return <div className="py-4">LOADING....</div>;
+  }
+
+  if (isError || !expensesData) {
+    return (
+      <div className="text-center text-red-500 py-4">Can't get expenses</div>
+    );
+  }
+
+  return (
+    <div>
+      {/* HEADER */}
+      <div className="mb-5">
+        <Header name="Expenses" />
+        <p className="text-sm text-gray-500">
+          A visual representation of expenses over time.
+        </p>
+      </div>
+      {/* FILTERS */}
+      <div className="flex flex-col md:flex-row justify-between gap-4">
+        <div className="w-full md:w-1/3 bg-white shadow rounded-lg p-6">
+          <h3 className="text-lg font-semibold mb-4">
+            Filter by Category and Date
+          </h3>
+          <div className="space-y-4">
+            {/* CATEGORY */}
+            <div>
+              <label htmlFor="category" className={classNames.label}>
+                Category
+              </label>
+              <select
+                id="category"
+                name="category"
+                className={classNames.selectInput}
+                defaultValue="All"
+                onChange={(e) => setSelectedCategory(e.target.value)}
+              >
+                <option>All</option>
+                <option>Office</option>
+                <option>Professional</option>
+                <option>Sales</option>
+              </select>
+            </div>
+            {/*START DATE */}
+            <div>
+              <label htmlFor="start-date" className={classNames.label}>
+                Start Date
+              </label>
+              <input
+                type="date"
+                id="start-date"
+                name="start-date"
+                className={classNames.selectInput}
+                onChange={(e) => setStartDate(e.target.value)}
+              />
+            </div>
+            {/*END DATE */}
+            <div>
+              <label htmlFor="end-date" className={classNames.label}>
+                End Date
+              </label>
+              <input
+                type="date"
+                id="end-date"
+                name="end-date"
+                className={classNames.selectInput}
+                onChange={(e) => setEndDate(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Expenses;
