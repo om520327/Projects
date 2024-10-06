5d6b85a7d124ba1b00b7e7dd467e47fdcd4e7078
SocialShams <omar.shams@socialstore.app>
2024-10-06 15:45:32 -0300

    pytest marking

--- Learning/Pytest/tests/test_functions.py
+++ Learning/Pytest/tests/test_functions.py
@@ -1,5 +1,6 @@
 import pytest
 import src.functions as functions 
+import time 
 
 
 #assert only lets program cont if it is true, otherwise assertion error is raised which stops program
@@ -20,3 +21,25 @@ def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError):
         functions.divide(10,0)
 
+#pytest Marking is a way to add metadata to tests (tagging ur tests with labels that can influence how the tests run ect)
+#can label some tests as 'slow' or 'skip'
+
+#SLOW TAG
+@pytest.mark.slow
+def test_slow():
+    time.sleep(5)
+    result = functions.divide(10,5)
+    assert result == 2
+#we can run test that only have a spicific tag by 'pytest -m tagname(in this case 'slow')'
+#we can run test that DONT have a spicific tag by 'pytest -m "not slow"' (this will run all tests without the tagname slow)
+
+#.skip mark will auto skip this test
+@pytest.mark.skip(reason="you can also add reasons for speicifc marks")
+def test_add_skip():
+    assert functions.add(1,2) == 3
+
+#xfail tag means expected fail, you can also add a reason
+@pytest.mark.xfail(reason="trying to divide by 0 when we know we cannot")
+def test_divide_xfail():
+     functions.divide(10,0)
+    
