681706dd6588c7c6b866cc42d566b49a53c011ff
SocialShams <omar.shams@socialstore.app>
2024-10-06 14:04:48 -0300

    class based testing

--- Learning/Pytest/tests/test_functions.py
+++ Learning/Pytest/tests/test_functions.py
@@ -6,3 +6,17 @@ import src.functions as functions
 def test_add():
     result = functions.add(1,4)
     assert result == 5
+
+def test_add_string():
+    result = functions.add("Lets See ", "What Happens")
+    assert result == "Lets See What Happens"
+
+def test_divide():
+    result = functions.divide(10,5)
+    assert result == 2
+
+#will test that the error is what you get
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        functions.divide(10,0)
+
