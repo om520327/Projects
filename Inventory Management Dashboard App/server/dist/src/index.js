428ed7527d0b9dd56c9ef855c1248bc09006d6da
SocialShams <omar.shams@socialstore.app>
2024-10-15 15:30:30 -0300

    AWS EC2 Config

--- Inventory Management Dashboard App/server/dist/src/index.js
+++ Inventory Management Dashboard App/server/dist/src/index.js
@@ -29,7 +29,7 @@ app.use("/dashboard", dashboardRoutes_1.default); //http:localhost:8000/dashboar
 app.use("/users", userRoutes_1.default); //http:localhost:8000/users
 app.use("/expenses", expenseRoutes_1.default); //http:localhost:8000/expenses
 /* SERVER */
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
     console.log(` Server running on port: ${port}`);
 });
