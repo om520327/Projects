745f70d235952dad54f52362580857a1001ab739
SocialShams <omar.shams@socialstore.app>
2024-10-28 15:37:34 -0300

    setting up chatbot view

--- Python(Django)ChatGPT/django_chatgpt/chatbot/views.py
+++ Python(Django)ChatGPT/django_chatgpt/chatbot/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+#we specified in setting.py what directory to use for our templates 
+#so our program knows the location of where chatbot.html is 
+def chatbot(request):
+    return render(request,'chatbot.html')
--- Python(Django)ChatGPT/django_chatgpt/django_chatgpt/urls.py
+++ Python(Django)ChatGPT/django_chatgpt/django_chatgpt/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+#include allows us to use urlpatters that are already defined in an app
+#in our case the chatbot folder url file
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    #now when someone wants to accsses the homepage, the program will match
+    #"''" field in chatbot folder urls file to know what to show.
+    path('', include('chatbot.urls')),
 ]
