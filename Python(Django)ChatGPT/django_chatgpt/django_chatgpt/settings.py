cc1f97d353f518229b330151e4b0fa1253c2bfec
SocialShams <omar.shams@socialstore.app>
2024-10-28 15:20:41 -0300

    setting up django project

--- Python(Django)ChatGPT/django_chatgpt/django_chatgpt/settings.py
+++ Python(Django)ChatGPT/django_chatgpt/django_chatgpt/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "django_chatgpt.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR,'html-template'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
