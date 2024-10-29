7d5142f05ff4d19fc7a3ccb98b49cf7be6f31c0b
SocialShams <omar.shams@socialstore.app>
2024-10-29 13:42:14 -0300

    testing chatbox funcationality works without AI API

--- Python(Django)ChatGPT/django_chatgpt/chatbot/views.py
+++ Python(Django)ChatGPT/django_chatgpt/chatbot/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 #we specified in setting.py what directory to use for our templates 
 #so our program knows the location of where chatbot.html is 
 def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = 'test test'
+        return JsonResponse({'messsage':message, 'response':response})
+
     return render(request,'chatbot.html')
