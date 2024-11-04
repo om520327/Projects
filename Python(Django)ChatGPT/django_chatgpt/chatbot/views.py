a36097f9fba225b29bba0b5312c02851e691e373
SocialShams <omar.shams@socialstore.app>
2024-11-04 16:30:49 -0400

    register page functionality done

--- Python(Django)ChatGPT/django_chatgpt/chatbot/views.py
+++ Python(Django)ChatGPT/django_chatgpt/chatbot/views.py
@@ -1,11 +1,12 @@
 import os
 import openai
 from openai.types.chat import ChatCompletion
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import JsonResponse
 from dotenv import load_dotenv
 from openai import OpenAI
 from django.contrib import auth
+from django.contrib.auth.models import User
 
 ### Since we are using LLM STUDIO set up is a little different than openai ###
 # load_dotenv()
@@ -54,8 +55,34 @@ def chatbot(request):
 def login(request):
     return render(request,'login.html')
 
-
+# want to add cred to db and auto log user in
+# in register.html we have a form for users cred
+# once register button is clicked, that info is sent here in "request"
+# we want to extract that info in order to store it in out db for future login
 def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 ==  password2:
+            try:
+                # user is created and saved
+                user = User.objects.create_user(username, email, password1)
+                user.save()
+                # now we log user in automatically after  registration 
+                auth.login(request, user)
+                # take user back to chatbot
+                return redirect('chatbot')
+            except:
+                error_message = 'Error creating account'
+                return render(request,'register.html', {'error_message':error_message})
+        else:
+            error_message = 'Passwords dont match'
+            return render(request,'register.html', {'error_message':error_message})
+
+
     return render(request,'register.html')
 
 
