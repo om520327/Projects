91b37f263ab6d41e82ced88c115515ee46c90c5f
SocialShams <omar.shams@socialstore.app>
2024-11-14 15:34:14 -0400

    registering Post model on admin page

--- Learning/DjangoTheoryPrac/PART1/newconcept/posts/admin.py
+++ Learning/DjangoTheoryPrac/PART1/newconcept/posts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Post 
 # Register your models here.
+# must register each model for each app in projecty in order for it to be shown on admin page
+
+admin.site.register(Post)
