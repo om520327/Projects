790bfa135dd4208f280b087674e613dcf6c9087e
SocialShams <omar.shams@socialstore.app>
2024-11-18 16:01:35 -0400

    using named url link

--- Learning/DjangoTheoryPrac/PART1/newconcept/posts/urls.py
+++ Learning/DjangoTheoryPrac/PART1/newconcept/posts/urls.py
@@ -5,9 +5,9 @@ from . import views
 # must let django know where this folder is to use ur html files.
 # once you do it you have accsses to all template folders within your project
 # that is why we dont have to do anything extra for posts app template folder
-
+# by adding the name field we can now use the named URL when reffering to this adress(look in layout.html)
 urlpatterns = [
-    path('', views.posts_list),
+    path('', views.posts_list, name="posts"),
 ]
 
 # since this is url patterns from a app within our project 
